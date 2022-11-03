339b5b144ec5c8b7a79c4c6e7895ce52b696361e
kishor <peddintikishor3@gmail.com>
2022-11-03 16:10:40 +0530

    first day programs

---
+++ problem2.js
@@ -0,0 +1,6 @@
+let name="kishor";
+console.log(name);
+name="suryanarayana";
+console.log(name);
+name="muthyalamma";
+console.log(name);
---
+++ problem4.js
@@ -0,0 +1,10 @@
+console.log( name="studentName   :sai");
+console.log( school="school        :delhi public school");
+console.log( grade="grade         :B");
+console.log( section="section       :A");
+console.log(rollno="roll_num      :10");
+
+console.log("subject   marks");
+console.log( marks_in_english="english    80");
+console.log(marks_in_maths="maths      90");
+console.log( marks_in_sanskrit="sanskrit   85");
